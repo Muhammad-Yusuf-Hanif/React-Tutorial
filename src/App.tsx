a4e8d1c684762ae1fcc18d507673211535cd1c3a
Muhammad-Yusuf-Hanif <yusufhanif786@hotmail.co.uk>
2024-04-29 17:12:58 +0100

    removed cloned counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import Counter from "./miniProjects/Counter/Counter";
-import CounterClone from "./miniProjects/CounterClone/CounterClone";
 import ToDoApp from "./miniProjects/Todoapp/ToDoApp";
 import API from "./miniProjects/WeatherAPI/API";
 
 function App() {
 	return (
 		<>
-			<Counter/>
-			<ToDoApp/>
-			<API/>
-			<CounterClone />
+			<Counter />
+			<ToDoApp />
+			<API />
 		</>
 	);
 }
